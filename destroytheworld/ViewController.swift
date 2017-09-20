509e0c094793432d3e1c3aa55a1c4e3920f187a6
Nithin Kambam <nithin@Nithins-MacBook-Pro.local>
2017-09-20 10:57:03 -0500

    Initial Commit

---
+++ destroytheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroytheworld
+//
+//  Created by Nithin Reddy Kambam on 9/20/17.
+//  Copyright © 2017 Nithin Reddy Kambam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
